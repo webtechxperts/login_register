0c72d4e010a0a0541564172637500d2e5e6261b9
Mukesh Kumar <webtechxpertscare@gmail.com>
2019-03-13 21:00:12 +0530

    update database function
    Update database function file

--- functions/db.php
+++ functions/db.php
@@ -5,13 +5,42 @@
 $con = mysqli_connect('localhost', 'root', '', 'login_db');
 
 
-function query($query){
-    
+// Escape Function for database
+
+function escape($string){
     global $con;
-    
-    return mysqli_query($con, $query);
+    return mysqli_real_escape_string($con, $string)
     
 }
+
+
+// Query Function for database
+
+function query($query){    
+    global $con;    
+    return mysqli_query($con, $query);    
+}
+
+
+// Confirm Function for database
+
+function confirm($result){
+    global $con;
+    if(!$result){
+        die( "QUERY FAILED", mysqli_error($con))
+    }
+}
+
+
+// Fetch Function for database
+
+function fetch_array($result){
+    global $con;
+    return mysqli_fetch_array($result)
+    
+}
+
+
 
 
 
